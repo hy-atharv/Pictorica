5129830982ece386efb1e72623bde14b4e19a6c3
Atharv Kumar Tiwari <131706297+hy-atharv@users.noreply.github.com>
2024-08-06 14:18:29 +0530

    Update firebaseConfig.js

--- Firebase/firebaseConfig.js
+++ Firebase/firebaseConfig.js
@@ -5,16 +5,8 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore, initializeFirestore } from "firebase/firestore";
 
-//const firebaseConfig = process.env.FIREBASE_CONFIG
- const firebaseConfig = {
-  apiKey: "AIzaSyDepS4_CjimZM89zLHTqT-7g-FKxQXKavQ",
-  authDomain: "pictorica-fe97f.firebaseapp.com",
-  projectId: "pictorica-fe97f",
-  storageBucket: "pictorica-fe97f.appspot.com",
-  messagingSenderId: "742728956413",
-  appId: "1:742728956413:web:a052dcf5b57d5338b84194",
-  measurementId: "G-WBY1PV5H02"
-}
+const firebaseConfig = process.env.FIREBASE_CONFIG
+
 
 
 
